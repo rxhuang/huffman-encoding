ba76a7dbff03f0f8cb6f297e3fe570de25408369
Alvin <wu.alvin99@gmail.com>
2017-02-15 17:22:14 -0800

    Checkpoint

--- uncompress.cpp
+++ uncompress.cpp
@@ -11,45 +11,41 @@ int main(int argc, char*argv[]){
     return -1;
   }
 
-  ofstream output;
-  ifstream input;
+  ofstream outfile;
+  ifstream infile;
   int size = 0;
-  int b;
+  byte b;
   string str;
 
   vector<int> freq(256, 0);
 
   //1. Open the input file for reading.
-  input.open(argv[1], ios::binary);
+  infile.open(argv[1], ios::binary);
 
 
-  input.seekg(0, ios::beg);
-  output.open(argv[2], ios::binary);
+  infile.seekg(0, ios::beg);
+  outfile.open(argv[2], ios::binary);
 
   //2. Read the file header at the beginning of the input file,
   //and reconstruct the Huffman coding tree.
   for (int i = 0; i < freq.size(); i++){
-    getline(input,str);
+    getline(infile,str);
     freq[i] = stoi(str);
     size += freq[i];
   }
-
+  // build the tree
   HCTree tree;
   tree.build(freq);
 
-
-  //find the begining of the file past the header
-  //input.seekg(ios::beg+(256*sizeof(int)));
   //4. Using the Huffman coding tree, decode the bits from the input file
   //into the appropriate sequence of bytes, writing them to the output file
   for (int x = 0; x < size; x++){
-    b = tree.decode(input);
-    output <<b;
+    b = tree.decode(infile);
+    outfile <<b;
   }
-
   //5. Close the input and output files.
-  output.close();
-  input.close();
+  outfile.close();
+  infile.close();
 
   return 0;
 }
